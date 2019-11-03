0279f7fd6965edae8a308995d1087702054d62b6
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-11-03 12:04:48 -0600

    Finish the model and crud operations

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+// LOCAL
+const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser to use req.body
+app.use(express.json());
 
 // Dev Logging Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -20,4 +29,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, () =>
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise refections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	// Close server & exit process
+	server.close(() => process.exit(1));
+});
